801810e1dc11e7569634e47b28a88ba22bb7654f
Michael Smith <droidvoider@gmail.com>
2017-02-25 00:44:21 -0800

    Update getroot.h
    Added definitions removed from iovroot back to getroot.h so that it will compile.

--- jni/include/getroot.h
+++ jni/include/getroot.h
@@ -6,12 +6,20 @@
 
 int read_at_address_pipe(void* address, void* buf, ssize_t len);
 int write_at_address_pipe(void* address, void* buf, ssize_t len);
+#ifdef __GNUC_GNU_INLINE__
 inline int writel_at_address_pipe(void* address, unsigned long val);
+#else
+extern inline int writel_at_address_pipe(void* address, unsigned long val);
+#endif
 unsigned int execute_via_check_flags(int dev, void* check_flags, void* function, unsigned long arg);
 unsigned long execute_via_task_prctl(struct offsets* o, void* function, int arg0, unsigned long long arg1, unsigned long long arg2, unsigned long long arg3, unsigned long long arg4, unsigned long long arg5);
 int modify_task_cred_rkp(struct thread_info* info);
 int disable_lkmauth(struct offsets *o);
 int disable_selinux_hooks(struct offsets *o);
+int modify_task_cred_uc(struct thread_info* info);
+//32bit
+struct thread_info* patchaddrlimit();
+//64bit
 void preparejop(void** addr, void* jopret);
 
 #define MMAP_START ((void*)0x40000000)
